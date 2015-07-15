05843b1448c30ab3c1de3937b884d09a1ca85c3f
Dima Kovalchuk <kovaldn@gmail.com>
2015-07-15 13:03:59 +0300

    done

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 > КАК УСТАНОВИТЬ МОЙ ПРОЕКТ
 
-1. git clone https://github.com/kovaldn/workflow1.git site
+1. git clone https://github.com/kovaldn/spinner.git site
 2. cd site
 3. bower i
 4. npm i
+5. gulp
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,23 @@
-document.write('ПРИВЕТ! Я РАБОТАЮ!!!!');
+$(document).ready(function() {
+	var spinner = $("#spinner").spinner({
+				'min' : 0
+			});
+			myFriend = $(".acdc"),
+			currentPosX = myFriend.css("left"),
+			currentPosXint = parseInt(currentPosX, 10);
+
+	spinner.on( "spin", function( event, ui ) {
+
+		// текущее значение спиннера
+		var currentVal = ui.value;
+
+		if (currentPosXint == currentVal){ // останавливаем
+			return;
+		} else if (currentPosXint < currentVal) { // двиваем вправо
+			currentPosXint = currentPosXint + 1;
+		} else {
+			currentPosXint = currentPosXint - 1; // двиваем влево
+		}
+		myFriend.css("left", currentPosXint);
+	});
+});
